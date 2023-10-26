217492623ed9fe4237a49ead55bc859ecdc068ec
Bohdan <bohdanrubell@gmail.com>
2023-10-26 20:34:02 +0300

    Added DBContext And create a database with 1 table

---
+++ CarribaVilla_ASP_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CarribaVilla_ASP_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarribaVilla_ASP_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+    }
+}
